1bc350a8ac3c902cbc48798d4a14a8592a234a34
Michael Damatov <michael.damatov@prodot.de>
2016-01-23 19:55:56 +0100

    update version to use the semantic versioning

--- Sources/ReCommendedExtension/Properties/AssemblyInfo.cs
+++ Sources/ReCommendedExtension/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using ReCommendedExtension;
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
