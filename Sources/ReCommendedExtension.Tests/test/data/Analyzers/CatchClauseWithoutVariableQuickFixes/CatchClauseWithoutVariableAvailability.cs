f1c7dd187240d54d93b1ed5cb82d597490c2eca9
Michael Damatov <michael.damatov@prodot.de>
2018-01-16 00:47:39 +0100

    add analyzer and quick fix to remove redundant exception type without exception variable
    #8

---
+++ Sources/ReCommendedExtension.Tests/test/data/Analyzers/CatchClauseWithoutVariableQuickFixes/CatchClauseWithoutVariableAvailability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test
+{
+    internal class CatchClauseWithoutVariable
+    {
+        void Method()
+        {
+            try { }
+            catch (Exception) { }
+
+            try { }
+            catch (Exception) when (true) { }
+
+            try { }
+            catch (Exception e) { }
+
+            try { }
+            catch (Exception e) when (e.Message != null) { }
+
+            try { }
+            catch (InvalidOperationException) { }
+        }
+    }
+}
