59699585adc16fd84abec2ded75518eb6c9a4138
Michael Damatov <michael.damatov@prodot.de>
2018-01-15 23:37:37 +0100

    add analyzer to detect local suppression
    #17

---
+++ Sources/ReCommendedExtension/Analyzers/LocalSuppressionAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using JetBrains.Annotations;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.Tree;
+using ReCommendedExtension.Highlightings;
+
+namespace ReCommendedExtension.Analyzers
+{
+    [ElementProblemAnalyzer(typeof(ICommentNode), HighlightingTypes = new[] { typeof(LocalSuppressionHighlighting) })]
+    public sealed class LocalSuppressionAnalyzer : ElementProblemAnalyzer<ICommentNode>
+    {
+        static int GetLeadingWhitespaceCharacterCount([NotNull] string commentText)
+        {
+            var leadingWhitespaceCharacters = 0;
+
+            for (var i = 0; i < commentText.Length; i++)
+            {
+                switch (commentText[i])
+                {
+                    case ' ':
+                    case '\t':
+                        leadingWhitespaceCharacters++;
+                        break;
+
+                    default:
+                        return leadingWhitespaceCharacters;
+                }
+            }
+
+            return leadingWhitespaceCharacters;
+        }
+
+        protected override void Run(ICommentNode element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            var leadingWhitespaceCharacterCount = GetLeadingWhitespaceCharacterCount(element.CommentText);
+
+            var commentText = element.CommentText.Remove(0, leadingWhitespaceCharacterCount);
+
+            if (commentText.StartsWith("ReSharper disable once", StringComparison.Ordinal))
+            {
+                consumer.AddHighlighting(new LocalSuppressionHighlighting("Local suppression.", element, leadingWhitespaceCharacterCount, true));
+            }
+            else
+            {
+                if (commentText.StartsWith("ReSharper disable", StringComparison.Ordinal))
+                {
+                    consumer.AddHighlighting(new LocalSuppressionHighlighting("Local suppression.", element, leadingWhitespaceCharacterCount, false));
+                }
+            }
+        }
+    }
+}
