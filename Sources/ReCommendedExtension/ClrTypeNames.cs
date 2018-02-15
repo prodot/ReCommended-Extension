9ceaa43156075800dc0ae77d355d7e977cdb4b7c
Michael Damatov <michael.damatov@prodot.de>
2018-02-15 22:10:29 +0100

    add analyzer to check whether objects with weak identity are locked (CA2002)
    #35

--- Sources/ReCommendedExtension/ClrTypeNames.cs
+++ Sources/ReCommendedExtension/ClrTypeNames.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
+using System.Reflection;
 using System.Windows.Data;
 using JetBrains.Annotations;
 using JetBrains.Metadata.Reader.API;
@@ -40,6 +41,23 @@ namespace ReCommendedExtension
 
         [NotNull]
         internal static readonly IClrTypeName IEqualityComparer = new ClrTypeName(typeof(IEqualityComparer).FullName.AssertNotNull());
+
+        [NotNull]
+        internal static readonly IClrTypeName MemberInfo = new ClrTypeName(typeof(System.Reflection.MemberInfo).FullName.AssertNotNull());
+
+        [NotNull]
+        internal static readonly IClrTypeName ParameterInfo = new ClrTypeName(typeof(ParameterInfo).FullName.AssertNotNull());
+
+        [NotNull]
+        internal static readonly IClrTypeName OutOfMemoryException = new ClrTypeName(typeof(OutOfMemoryException).FullName.AssertNotNull());
+
+        [NotNull]
+        internal static readonly IClrTypeName StackOverflowException = new ClrTypeName(typeof(StackOverflowException).FullName.AssertNotNull());
+
+        [NotNull]
+#pragma warning disable 618 // obsolete type
+        internal static readonly IClrTypeName ExecutionEngineException = new ClrTypeName(typeof(ExecutionEngineException).FullName.AssertNotNull());
+#pragma warning restore 618
 
         [NotNull]
         internal static readonly IClrTypeName MultiBinding = new ClrTypeName(typeof(MultiBinding).FullName.AssertNotNull());
