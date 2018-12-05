f71bd1d9649b4132b1e656b7861f389592ebf92e
Michael Damatov <michael.damatov@prodot.de>
2018-12-05 19:08:17 +0100

    improve detection of MSTest method attributes

--- Sources/ReCommendedExtension.Tests/test/data/Analyzers/Await/RedundantAwait_TestProject.cs
+++ Sources/ReCommendedExtension.Tests/test/data/Analyzers/Await/RedundantAwait_TestProject.cs
@@ -4,10 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ReCommendedExtension.Tests.test.data.Analyzers.Await
 {
-    public class AwaitForMethods
+    public class TestMethods
     {
         [TestMethod]
-        async Task Method()
+        public async Task Method()
         {
             if (Environment.UserInteractive)
             {
@@ -18,7 +18,7 @@ namespace ReCommendedExtension.Tests.test.data.Analyzers.Await
         }
 
         [TestMethod]
-        async Task Method_WithConfigureAwait()
+        public async Task Method_WithConfigureAwait()
         {
             if (Environment.UserInteractive)
             {
@@ -29,29 +29,337 @@ namespace ReCommendedExtension.Tests.test.data.Analyzers.Await
         }
 
         [TestMethod]
-        async Task Method_AsExpressionBodied() => await Task.Delay(10);
+        public async Task Method_AsExpressionBodied() => await Task.Delay(10);
 
         [TestMethod]
-        async Task Method_AsExpressionBodied_WithConfigureAwait() => await Task.Delay(10).ConfigureAwait(false);
+        public async Task Method_AsExpressionBodied_WithConfigureAwait() => await Task.Delay(10).ConfigureAwait(false);
 
         [TestMethod]
-        async Task Method4()
+        public async Task Method4()
         {
             Console.WriteLine();
             await Task.FromResult("one");
         }
 
         [TestMethod]
-        async Task Method4_WithConfigureAwait()
+        public async Task Method4_WithConfigureAwait()
         {
             Console.WriteLine();
             await Task.FromResult("one").ConfigureAwait(false);
         }
 
         [TestMethod]
-        async Task Method4_AsExpressionBodied() => await Task.FromResult("one");
+        public async Task Method4_AsExpressionBodied() => await Task.FromResult("one");
 
         [TestMethod]
-        async Task Method4_AsExpressionBodied_WithConfigureAwait() => await Task.FromResult("one").ConfigureAwait(false);
+        public async Task Method4_AsExpressionBodied_WithConfigureAwait() => await Task.FromResult("one").ConfigureAwait(false);
+    }
+
+    public class TestInitializers
+    {
+        [TestInitialize]
+        public async Task Method()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10);
+        }
+
+        [TestInitialize]
+        public async Task Method_WithConfigureAwait()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+
+        [TestInitialize]
+        public async Task Method_AsExpressionBodied() => await Task.Delay(10);
+
+        [TestInitialize]
+        public async Task Method_AsExpressionBodied_WithConfigureAwait() => await Task.Delay(10).ConfigureAwait(false);
+
+        [TestInitialize]
+        public async Task Method4()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one");
+        }
+
+        [TestInitialize]
+        public async Task Method4_WithConfigureAwait()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one").ConfigureAwait(false);
+        }
+
+        [TestInitialize]
+        public async Task Method4_AsExpressionBodied() => await Task.FromResult("one");
+
+        [TestInitialize]
+        public async Task Method4_AsExpressionBodied_WithConfigureAwait() => await Task.FromResult("one").ConfigureAwait(false);
+    }
+
+    public class ClassInitializers
+    {
+        [ClassInitialize]
+        public static async Task Method(TestContext testContext)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10);
+        }
+
+        [ClassInitialize]
+        public static async Task Method_WithConfigureAwait(TestContext testContext)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+
+        [ClassInitialize]
+        public static async Task Method_AsExpressionBodied(TestContext testContext) => await Task.Delay(10);
+
+        [ClassInitialize]
+        public static async Task Method_AsExpressionBodied_WithConfigureAwait(TestContext testContext) => await Task.Delay(10).ConfigureAwait(false);
+
+        [ClassInitialize]
+        public static async Task Method4(TestContext testContext)
+        {
+            Console.WriteLine();
+            await Task.FromResult("one");
+        }
+
+        [ClassInitialize]
+        public static async Task Method4_WithConfigureAwait(TestContext testContext)
+        {
+            Console.WriteLine();
+            await Task.FromResult("one").ConfigureAwait(false);
+        }
+
+        [ClassInitialize]
+        public static async Task Method4_AsExpressionBodied(TestContext testContext) => await Task.FromResult("one");
+
+        [ClassInitialize]
+        public static async Task Method4_AsExpressionBodied_WithConfigureAwait(TestContext testContext)
+            => await Task.FromResult("one").ConfigureAwait(false);
+    }
+
+    public class AssemblyInitializers
+    {
+        [AssemblyInitialize]
+        public static async Task Method(TestContext testContext)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10);
+        }
+
+        [AssemblyInitialize]
+        public static async Task Method_WithConfigureAwait(TestContext testContext)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+
+        [AssemblyInitialize]
+        public static async Task Method_AsExpressionBodied(TestContext testContext) => await Task.Delay(10);
+
+        [AssemblyInitialize]
+        public static async Task Method_AsExpressionBodied_WithConfigureAwait(TestContext testContext) => await Task.Delay(10).ConfigureAwait(false);
+
+        [AssemblyInitialize]
+        public static async Task Method4(TestContext testContext)
+        {
+            Console.WriteLine();
+            await Task.FromResult("one");
+        }
+
+        [AssemblyInitialize]
+        public static async Task Method4_WithConfigureAwait(TestContext testContext)
+        {
+            Console.WriteLine();
+            await Task.FromResult("one").ConfigureAwait(false);
+        }
+
+        [AssemblyInitialize]
+        public static async Task Method4_AsExpressionBodied(TestContext testContext) => await Task.FromResult("one");
+
+        [AssemblyInitialize]
+        public static async Task Method4_AsExpressionBodied_WithConfigureAwait(TestContext testContext)
+            => await Task.FromResult("one").ConfigureAwait(false);
+    }
+
+    public class TestCleanups
+    {
+        [TestCleanup]
+        public async Task Method()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10);
+        }
+
+        [TestCleanup]
+        public async Task Method_WithConfigureAwait()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+
+        [TestCleanup]
+        public async Task Method_AsExpressionBodied() => await Task.Delay(10);
+
+        [TestCleanup]
+        public async Task Method_AsExpressionBodied_WithConfigureAwait() => await Task.Delay(10).ConfigureAwait(false);
+
+        [TestCleanup]
+        public async Task Method4()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one");
+        }
+
+        [TestCleanup]
+        public async Task Method4_WithConfigureAwait()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one").ConfigureAwait(false);
+        }
+
+        [TestCleanup]
+        public async Task Method4_AsExpressionBodied() => await Task.FromResult("one");
+
+        [TestCleanup]
+        public async Task Method4_AsExpressionBodied_WithConfigureAwait() => await Task.FromResult("one").ConfigureAwait(false);
+    }
+
+    public class ClassCleanups
+    {
+        [ClassCleanup]
+        public static async Task Method()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10);
+        }
+
+        [ClassCleanup]
+        public static async Task Method_WithConfigureAwait()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+
+        [ClassCleanup]
+        public static async Task Method_AsExpressionBodied() => await Task.Delay(10);
+
+        [ClassCleanup]
+        public static async Task Method_AsExpressionBodied_WithConfigureAwait() => await Task.Delay(10).ConfigureAwait(false);
+
+        [ClassCleanup]
+        public static async Task Method4()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one");
+        }
+
+        [ClassCleanup]
+        public static async Task Method4_WithConfigureAwait()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one").ConfigureAwait(false);
+        }
+
+        [ClassCleanup]
+        public static async Task Method4_AsExpressionBodied() => await Task.FromResult("one");
+
+        [ClassCleanup]
+        public static async Task Method4_AsExpressionBodied_WithConfigureAwait() => await Task.FromResult("one").ConfigureAwait(false);
+    }
+
+    public class AssemblyCleanups
+    {
+        [AssemblyCleanup]
+        public static async Task Method()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10);
+        }
+
+        [AssemblyCleanup]
+        public static async Task Method_WithConfigureAwait()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine();
+            }
+
+            await Task.Delay(10).ConfigureAwait(false);
+        }
+
+        [AssemblyCleanup]
+        public static async Task Method_AsExpressionBodied() => await Task.Delay(10);
+
+        [AssemblyCleanup]
+        public static async Task Method_AsExpressionBodied_WithConfigureAwait() => await Task.Delay(10).ConfigureAwait(false);
+
+        [AssemblyCleanup]
+        public static async Task Method4()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one");
+        }
+
+        [AssemblyCleanup]
+        public static async Task Method4_WithConfigureAwait()
+        {
+            Console.WriteLine();
+            await Task.FromResult("one").ConfigureAwait(false);
+        }
+
+        [AssemblyCleanup]
+        public static async Task Method4_AsExpressionBodied() => await Task.FromResult("one");
+
+        [AssemblyCleanup]
+        public static async Task Method4_AsExpressionBodied_WithConfigureAwait() => await Task.FromResult("one").ConfigureAwait(false);
     }
 }
