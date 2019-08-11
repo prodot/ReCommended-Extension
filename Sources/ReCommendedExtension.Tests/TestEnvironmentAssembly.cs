57dfda364299c310c617e8c805c52ef357aa040f
Michael Damatov <Michael.Damatov@prodot.de>
2019-08-11 18:22:08 +0200

    upgrade to ReSharper SDK 2019.2.0

--- Sources/ReCommendedExtension.Tests/TestEnvironmentAssembly.cs
+++ Sources/ReCommendedExtension.Tests/TestEnvironmentAssembly.cs
@@ -1,9 +1,10 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading;
 using JetBrains.TestFramework;
 using NUnit.Framework;
 using ReCommendedExtension.Tests;
 
-[assembly: RequiresSTA]
+[assembly: Apartment(ApartmentState.STA)]
 
 [SetUpFixture]
 [SuppressMessage("ReSharper", "CheckNamespace", Justification = "Must be in the global namespace.")]
