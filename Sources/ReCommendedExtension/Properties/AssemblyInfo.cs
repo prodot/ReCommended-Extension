c828d83cee08eed521312ecbd837227793cea897
Michael Damatov <Michael.Damatov@prodot.de>
2019-11-01 13:13:05 +0100

    update version

--- Sources/ReCommendedExtension/Properties/AssemblyInfo.cs
+++ Sources/ReCommendedExtension/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using ReCommendedExtension;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.6.4.0")]
-[assembly: AssemblyFileVersion("3.6.4")]
+[assembly: AssemblyVersion("3.6.5.0")]
+[assembly: AssemblyFileVersion("3.6.5")]
