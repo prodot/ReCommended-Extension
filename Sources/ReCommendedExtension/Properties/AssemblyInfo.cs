80b4d77104dd4dfcf8eb6140275163ef66b11358
Michael Damatov <Michael.Damatov@prodot.de>
2022-01-31 18:03:32 +0100

    update copyright year

--- Sources/ReCommendedExtension/Properties/AssemblyInfo.cs
+++ Sources/ReCommendedExtension/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using ReCommendedExtension;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("prodot GmbH")]
 [assembly: AssemblyProduct(ZoneMarker.ExtensionId)]
-[assembly: AssemblyCopyright("© 2012-2021 prodot GmbH")]
+[assembly: AssemblyCopyright("© 2012-2022 prodot GmbH")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
