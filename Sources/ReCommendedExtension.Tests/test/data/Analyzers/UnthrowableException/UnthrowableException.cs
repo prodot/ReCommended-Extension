d463b7449c12a1e0f2f17161b0ae84cc84a6c7d5
Michael Damatov <michael.damatov@prodot.de>
2025-09-15 18:55:12 +0200

    improve exception detection: add WinUI exception types

--- Sources/ReCommendedExtension.Tests/test/data/Analyzers/UnthrowableException/UnthrowableException.cs
+++ Sources/ReCommendedExtension.Tests/test/data/Analyzers/UnthrowableException/UnthrowableException.cs
@@ -9,10 +9,39 @@ namespace Test
             throw new Exception();
         }
 
+        void Method(Exception e)
+        {
+            throw e;
+        }
+
         void Method2() => throw new Exception();
 
         string x;
 
         void Method3(string value) => x = value ?? throw new Exception();
+
+        void Method4()
+        {
+            try
+            {
+                Method();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        void Method5()
+        {
+            try
+            {
+                Method();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
